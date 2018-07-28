a0035d364677fcec8ab29ed0a7e595e4e95f121d
blinfoldking <ganeshadanu1@gmail.com>
2018-07-28 23:19:33 +0700

    extracting foreground from background

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+import imutils
 
 bg = None
 
@@ -11,6 +12,7 @@ def run_avg(image, aWeight):
         return
 
     cv2.accumulateWeighted(image, bg, aWeight)
+
 
 def segment(image, threshold=25):
     global bg
@@ -24,18 +26,18 @@ def segment(image, threshold=25):
     (_, cnts, _) = cv2.findContours(thresholded.copy(),
                                     cv2.RETR_EXTERNAL,
                                     cv2.CHAIN_APPROX_SIMPLE)
-    
+   
     if len(cnts) == 0:
         return
     else:
         segmented = max(cnts, key=cv2.contourArea)
-        return (cnts, segmented)
+        return (thresholded, segmented)
 
 
 # main function
 
 if __name__ == '__main__':
-    aWeight = 0,5
+    aWeight = 0.5
     cap = cv2.VideoCapture(0)
 
     top, right, bottom, left = 10, 350, 255, 590
@@ -44,22 +46,22 @@ if __name__ == '__main__':
 
     while True:
         ret, frame = cap.read()
-        frame = cv2.flip(frame, 1) 
+        frame = cv2.flip(frame, 1)
+        frame = imutils.resize(frame, width=700)
         clone = frame.copy()
         (height, width) = frame.shape[:2]
         roi = frame[top:bottom, right:left]
         gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
         gray = cv2.GaussianBlur(gray, (7, 7), 0)
-        
         if num_frams < 30:
             run_avg(gray, aWeight)
         else:
-            hand = segment(gray)
+            hand = segment(gray, 15)
             if hand is not None:
                 (thresholded, segmented) = hand
                 cv2.drawContours(clone, [segmented + (right, top)], -1, (0, 0, 255))
                 print(thresholded)
-                cv2.imshow("Threholded", thresholded)
+                #cv2.imshow("Threholded", thresholded)
         
         cv2.rectangle(clone, (left, top), (right, bottom), (0, 255, 0), 2)
         num_frams += 1
